e20c5c5c9e6e89ac9fdfef29d44fdb720ece5b3d
jdiveley@diveley.net <jdiveley@diveley.net>
2025-07-01 00:55:37 +0000

    first comit

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "/React_Vite_CICD_Github/",
+  server: {
+    open: true,
+    port: 3001,
+    allowedHosts: 'webdev.diveley.net',
+  },
+});
